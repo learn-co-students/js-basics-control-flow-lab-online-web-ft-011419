57263d134a6a2fc70c6b5321a9c059cd024ca011
grangerl330 <grangerl330@gmail.com>
2019-04-18 11:20:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let result;
+  if (distance <= 400) {
+    result = "This one is on me!";
+  } else if (distance > 2000 && distance <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    result = "No can do.";
+  }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  let result;
+  city === "NYC" ? result = "Ok, sounds good." : result = "No go.";
+  return result
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case "generous":
+    return "Thank you so much."
+    break;
+    case "not as generous":
+    return "Thank you."
+    default:
+    return "Bye."
+  }
+}
